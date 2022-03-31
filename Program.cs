6829c08fd8aea8ecf277338f1e3608bc92a231fc
saurabhgogoi <101546679+saurabhgogoi@users.noreply.github.com>
2022-03-31 18:18:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandlingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHandling fileHandling = new FileHandling();
+            //fileHandling.WriteFile();
+            fileHandling.ReadFile();
+            Console.ReadLine();
+        }
+    }
+}
